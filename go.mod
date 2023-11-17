8789153c27c691d5cf45f12842010ec999ea3794
THALOUM <amr31hassan3@gmail.com>
2023-11-17 02:18:35 +0200

    add the dog repo to the mod and using the WhenGrownUp function from it

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/THALOUM/Puppy
 
 go 1.21.4
+
+require github.com/THALOUM/dog v0.0.0-20231117001254-c4651cf6bad5 // indirect
--- puppy.go
+++ puppy.go
@@ -1,5 +1,13 @@
 package puppy
 
+import (
+	"github.com/THALOUM/dog"
+)
+
 func Bark() string {
 	return "woof!"
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Bark())
 }
